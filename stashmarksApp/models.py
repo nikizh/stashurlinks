7c44c1f80c09ffa78a982e56042f6b5967f3a812
Nikolay Zhekov <nikizh@gmail.com>
2015-02-01 10:20:09 +0000

    Added model classes
    Note: not migrated

--- stashmarksApp/models.py
+++ stashmarksApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=256)
+    url = models.URLField()
+    public = models.BooleanField(default=True)
+    thumb = models.CharField(max_length=128)
+    tags = models.ManyToManyField(Tag)
+    owner = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name
