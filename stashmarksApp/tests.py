89cdb797a5b32ac893ca56483cff8242cacf0eec
Svetoslav Petsov <spsetsov@gmail.com>
2015-03-21 17:13:48 +0000

    tests for models added

--- stashmarksApp/tests.py
+++ stashmarksApp/tests.py
@@ -1,3 +1,20 @@
-#from django.test import TestCase
+from django.test import TestCase
+from django.contrib.auth.models import User
+import stashmarksApp.models as models
 
+class ModelTests(TestCase):
 
+    def test_ensure_tag_constructor(self):
+        tag = models.Tag(name="Test")
+        tag.save()
+        self.assertEqual(tag.name, "Test")
+
+    def test_ensure_bookmark_constructor(self):
+        testUser = User()
+        testUser.save()
+        bm = models.Bookmark(title="Test",public=False, likes=1, owner=testUser)
+        bm.save()
+        self.assertEqual(bm.title, "Test")
+        self.assertEqual(bm.public, False)
+        self.assertEqual(bm.likes, 1)
+        self.assertEqual(bm.owner,testUser)
